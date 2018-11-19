32a0556dae5154317c113d62a73ef573c4e0d8e8
yongchao zhou <yongchao.zhou@mail.utoronto.ca>
2018-11-19 11:11:23 -0500

    add print_flags

--- utils/misc.py
+++ utils/misc.py
@@ -1,5 +1,15 @@
+import tensorflow as tf
+import sys
 from pprint import pprint
 
 
 def print_config(config):
+    print("##################   Current Config   ################")
     pprint({attr: config.__getattribute__(attr) for attr in dir(config) if not attr.startswith('__')})
+
+
+def print_flags(FLAGS):
+    print("##################   Current Flags   ################")
+    for key, value in tf.app.flags.FLAGS.flag_values_dict().items():
+        print("{}:  {}".format(key,value))
+        sys.stdout.flush()
