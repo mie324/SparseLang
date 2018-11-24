48e6d3bf2b4a0189d750055e9df736fb5c03d07b
yongchao zhou <yongchao.zhou@mail.utoronto.ca>
2018-11-24 15:07:26 -0500

    Add print_variable

--- utils/misc.py
+++ utils/misc.py
@@ -16,5 +16,12 @@ def print_flags(FLAGS):
         sys.stdout.flush()
 
 
+def print_variable():
+    all_var = tf.trainable_variables()
+    for var in all_var:
+        num = np.product([xi.value for xi in var.get_shape()])
+        print(var.name,num)
+    print("#########################")
+
 def get_num_para():
-    return np.sum([np.product([xi.value for xi in x.get_shape()]) for x in tf.all_variables()])
+    return np.sum([np.product([xi.value for xi in x.get_shape()]) for x in tf.trainable_variables()])
