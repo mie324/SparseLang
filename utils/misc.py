aa4e019ecdc622b242136b8cd15d21498fa72e86
yongchao zhou <yongchao.zhou@mail.utoronto.ca>
2018-11-24 13:25:32 -0500

    get_num_para

--- utils/misc.py
+++ utils/misc.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import sys
+import numpy as np
 from pprint import pprint
 
 
@@ -11,5 +12,9 @@ def print_config(config):
 def print_flags(FLAGS):
     print("##################   Current Flags   ################")
     for key, value in tf.app.flags.FLAGS.flag_values_dict().items():
-        print("{}:  {}".format(key,value))
+        print("{}:  {}".format(key, value))
         sys.stdout.flush()
+
+
+def get_num_para():
+    return np.sum([np.product([xi.value for xi in x.get_shape()]) for x in tf.all_variables()])
